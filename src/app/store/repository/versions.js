f4b23a11a79dc3395ccdb59af2ec5884435966df
ruhljeremias@gmail.com <ruhljeremias@gmail.com>
2025-02-28 11:30:44 +0100

    before optimisation 1

--- src/app/store/repository/versions.js
+++ src/app/store/repository/versions.js
@@ -12,10 +12,11 @@ export function readVersionFileZip(repo_id, version_id){
     return fs.readFileSync(getZipVersionPath(repo_id, version_id))
 }
 
-export function createVersionFile(repo_id, new_version_id, old_file_path, contributor){
+export function createVersionFile(repo_id, new_version_id, old_version_id, contributor){
     return fs.writeFileSync(`${root}/${repo_id}/history/${new_version_id}.version.json`,JSON.stringify({
         contributor: contributor,
-        changes: getDiff(new_version_id, old_file_path)
+        parents: [old_version_id],
+        changes: getDiff(new_version_id, old_version_id)
     }))
 }
 
@@ -27,7 +28,11 @@ export function createMergedVersionFile(repo_id, version_id, branch_version_id, 
     }))
 }
 
-export function createInitialVersionFile(repo_id, new_version_id, old_file_path, contributor){
+export function getParent(repo_id, version_id){
+    return JSON.parse(fs.readFileSync(`${root}/${repo_id}/history/${version_id}.version.json`))["parents"]
+}
+
+export function createInitialVersionFile(repo_id, new_version_id, contributor){
     return fs.writeFileSync(`${root}/${repo_id}/history/${new_version_id}.version.json`,JSON.stringify({
         contributor: contributor,
         changes: getDiff(new_version_id, null),
@@ -47,13 +52,13 @@ export function getZipVersionPath(repo_id, version_id){
     return `${root}/${repo_id}/cache/${version_id}.zip`
 }
 
-export function getVersionChangeChain(repo_id, version_id){
+export function getVersionChangeChain(repo_id, version_id, stop_version_id){
     let result = []
 
     function getVersionChanges(rec_version_id){
         const version_object = JSON.parse(fs.readFileSync(`${root}/${repo_id}/history/${rec_version_id}.version.json`))
         result.push(version_object["changes"])
-        if (version_object["merged"]===true|| version_object["init"]===true) {
+        if (version_object["merged"]===true|| version_object["init"]===true||rec_version_id===stop_version_id) {
             return {changes: result, base: rec_version_id};
         } else {
             getVersionChanges(version_object["parents"][0])
@@ -61,4 +66,18 @@ export function getVersionChangeChain(repo_id, version_id){
     }
 
     getVersionChanges(version_id)
+}
+
+export function getBranchOrigin(repo_id, main_version_id, branch_version_id){
+
+    let tmp_main_version_id = main_version_id;
+    let tmp_branch_version_id = branch_version_id;
+
+    while (tmp_branch_version_id !== tmp_main_version_id) {
+        tmp_branch_version_id = getParent(repo_id, tmp_branch_version_id)[0]
+
+        tmp_main_version_id = getParent(repo_id, tmp_main_version_id)[0]
+    }
+
+    return tmp_branch_version_id;
 }
--- src/app/zipWrapper/generateMergedZip.js
+++ src/app/zipWrapper/generateMergedZip.js
@@ -1,8 +1,12 @@
-import { getVersionChangeChain } from "../store/repository/versions";
+import { getBranchOrigin, getVersionChangeChain } from "../store/repository/versions";
 
 export function generateMergedZip(repo_id, version_id, branch_version_id, new_version_id){
 
-    getVersionChangeChain()
+    const main_changes = getVersionChangeChain(repo_id, version_id, getBranchOrigin(version_id))
+
+    const branch_changes = getVersionChangeChain(repo_id, branch_version_id, getBranchOrigin(branch_version_id))
+
+    
 
     // void -> create merged zip file -> {repo_id}/cache/{new_version_id}.zip
 }
